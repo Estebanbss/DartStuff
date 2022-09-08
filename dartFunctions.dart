dd8a4bacb4793fd4a21522434584f1a9e68d1131
Esteban Barrera <juanesbs2003@hotmail.com>
2022-09-08 11:20:09 -0500

    update to -dartFunctions-

--- dartFunctions.dart
+++ dartFunctions.dart
@@ -1,9 +1,12 @@
 void main() {
-  String mensaje = saludar(texto: "Hola,", nombre: "Juan");
-
-  print(mensaje);
+  String mensaje1 = saludar2(texto: "Hola,", nombre: "Juan");
+  String mensaje2 = saludar(texto: "Holas,", nombre: "Carla");
+  print(mensaje1);
+  print(mensaje2);
 }
 
 String saludar({String texto = "", String nombre = ""}) {
   return "$texto $nombre";
 }
+
+String saludar2({String texto = "", String nombre = ""}) => "$texto $nombre";
